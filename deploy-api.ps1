e84ec9f86849aee5f364fb5b316048090e7c4b91
Andy Blackledge <andyblackledge@hotmail.com>
2020-09-23 22:15:09 +0100

    Moving ever forward

--- deploy-api.ps1
+++ deploy-api.ps1
@@ -2,7 +2,7 @@ aws configure set region eu-west-2
 
 # TODO: Pass in the application name and iterate over the files
 
-aws s3 cp src\api\api.graphqls s3://agb-app-source/serverless-file-processing
-aws s3 cp src\api\getFirmAuthorisationRequest.vtl s3://agb-app-source/serverless-file-processing
-aws s3 cp src\api\getFirmAuthorisationResponse.vtl s3://agb-app-source/serverless-file-processing
+aws s3 cp src\api\api.graphqls s3://agb-app-source/serverless-file-processing/api.graphqls
+aws s3 cp src\api\getFirmAuthorisationRequest.vtl s3://agb-app-source/serverless-file-processing/getFirmAuthorisationRequest.vtl
+aws s3 cp src\api\getFirmAuthorisationResponse.vtl s3://agb-app-source/serverless-file-processing/getFirmAuthorisationResponse.vtl
 
