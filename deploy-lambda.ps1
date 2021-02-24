6a22062972742e7dfe3d91ee8aa6e76112cd3876
Andy Blackledge <andyblackledge@hotmail.com>
2021-02-24 19:10:17 +0000

    Added output

--- deploy-lambda.ps1
+++ deploy-lambda.ps1
@@ -15,7 +15,9 @@ $FunctionList = aws lambda list-functions | ConvertFrom-Json
 $Functions = $FunctionList.Functions
 $AppFunctions = $Functions | Where-Object {$_.Functionname -match "^" + $AppName + "-"}
 
+Write-Output "Updating lambdas..."
 $AppFunctions | Foreach-Object {
     aws lambda update-function-code --function-name $_.FunctionName --s3-bucket $S3Bucket --s3-key $S3Key | Out-Null
     Write-Output $_.FunctionName
 }
+Write-Output "Done"
